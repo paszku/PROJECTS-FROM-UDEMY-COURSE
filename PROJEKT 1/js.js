da78e76f5c7e632e4f082e4aa0b101aa7f217178
Konrad Paszek <paszku@icloud.com>
2020-03-23 20:18:53 +0100

    add styles and start JS

---
+++ PROJEKT 1/js.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+
+let clicks = 1;
+
+button.addEventListener("click", function () {
+    if (clicks % 5) {
+        document.createElement("div")
+
+    }
+})
