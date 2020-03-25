893c229d8af284c8675142c28a12efc219b7be6d
Konrad Paszek <paszku@icloud.com>
2020-03-25 10:38:32 +0100

    END PROJEKT 6

--- PROJEKT 6/main.js
+++ PROJEKT 6/main.js
@@ -2,11 +2,27 @@ let size = 10;
 let orderElement = 1;
 
 const init = () => {
- // tutaj kod
+    const btn = document.createElement('button');
+    btn.innerText = "Dodaj 10 elementów listy";
+    document.body.appendChild(btn)
+    btn.addEventListener('click', createLiElements)
+    const list = document.createElement('ul');
+    document.body.appendChild(list)
+
 }
 
 const createLiElements = () => {
- // tutaj kod
+    let i = 0;
+    for (i = 0; i < 10; i++) {
+        const newLi = document.createElement('li');
+        const list = document.querySelector('ul');
+        list.appendChild(newLi);
+        newLi.innerText = orderElement;
+        orderElement++;
+        newLi.style.fontSize = size + 'px';
+        size++
+    }
+    i = 0;
 }
 
 init()
